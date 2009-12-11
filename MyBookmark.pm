f75c7781b77cbb79952112b43737c080fd402a47
yappo <yappo@shibuya.pl>
2009-12-11 14:37:45 +0900

    add day 9

--- MyBookmark.pm
+++ MyBookmark.pm
@@ -17,13 +17,15 @@ use Data::Model::Mixin modules => ['+MyBookmark::Mixin::Count', 'FindOrCreate'];
 install_model user => schema {
     key 'id';
     unique 'nickname';
-    columns qw/ id nickname /;
+    column id => int => {};
+    columns qw/ nickname /;
 };
 
 install_model bookmark => schema {
     key [qw/ url_id user_id /];
     index 'user_id';
-    columns qw/ url_id user_id /;
+    column url_id => int => {};
+    column user_id => int => {};
 
     add_method user => sub {
         my $row = shift;
--- MyBookmark/Mixin/Count.pm
+++ MyBookmark/Mixin/Count.pm
@@ -2,6 +2,7 @@ package MyBookmark::Mixin::Count;
 use strict;
 use warnings;
 use Data::Model::SQL;
+use Data::Model::Driver::DBI;
 
 sub register_method {
     +{
@@ -10,15 +11,59 @@ sub register_method {
 }
 
 sub count {
-    my($self, $model) = @_;
+    my($self, $model, $key, $query) = @_;
+    my $driver = $self->get_driver($model);
+    my $schema = $self->get_schema($model);
 
-    my $dbh = $self->get_driver($model)->r_handle;
-    my $sth = $dbh->prepare_cached('SELECT COUNT(*) AS count FROM ' . $model);
-    $sth->execute;
-    my $count;
-    $sth->bind_columns(undef, \$count);
-    $count = 0 unless $sth->fetch;
-    $sth->finish;
+    if (defined $key && ref($key) eq '') {
+        # primary key は array-ref である必要がある
+        $key = [ $key ];
+    } elsif (ref($key) ne 'ARRAY') {
+        $query = $key;
+        undef $key;
+    }
+    unless (defined $query) {
+        $query = {};
+    } else {
+        die "query should be hash-ref" unless ref($query) eq 'HASH';
+    }
+
+    my %q = (
+        select => 'COUNT(*) AS count',
+        from   => $model,
+        %{ $query }
+    );
+    my $index = $q{index};
+    my $sql_obj = Data::Model::SQL->new( %q );
+    $driver->add_key_to_where($sql_obj, $schema->key, $key) if $key;
+    $driver->add_index_to_where($schema, $sql_obj, $index) if $index;
+    my $sql = $sql_obj->as_sql;
+
+    # パラメータをバインドする為の前処理
+    my @params;
+    for my $i (1..scalar(@{ $sql_obj->bind })) {
+        push @params, [ $sql_obj->bind_column->[$i - 1], $sql_obj->bind->[$i - 1] ];
+    }
+
+    my $sth;
+    my $count = 0;
+    eval {
+        # SELECT する為の dbh を取得
+        my $dbh = $driver->r_handle;
+        $driver->start_query($sql, $sql_obj->bind); # クエリログ用
+        $sth = $dbh->prepare($sql);
+        # パラメータを bind する
+        $driver->bind_params($schema, \@params, $sth);
+        $sth->execute;
+        $sth->bind_columns(undef, \$count);
+        $count = 0 unless $sth->fetch;
+        $sth->finish;
+        $driver->end_query($sth); # クエリログ用
+    };
+    if ($@) {
+        # エラーがあったらスタックトレース吐いて終了
+        $driver->_stack_trace($sth, $sql, $sql_obj->bind, $@);
+    }
     return $count;
 }
 
